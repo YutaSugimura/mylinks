57cda85bf58756f24bd9b0e77eb64c6ffd5ff312
YutaSugimura <nextc3m@gmail.com>
2021-04-25 14:43:45 +0900

    Update navigation/auth

--- src/navigation/auth.tsx
+++ src/navigation/auth.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { NavigationProp, RouteProp } from '@react-navigation/core';
 import { createStackNavigator } from '@react-navigation/stack';
 import AuthScreen from '../screens/auth';
+import SignupScreen from '../screens/auth/signup';
+import SigninScreen from '../screens/auth/signin';
 
 export type AuthStackParamList = {
   AuthTop: undefined;
+  Signup: undefined;
+  Signin: undefined;
 };
 
 export type AuthStackScreen = keyof AuthStackParamList;
@@ -14,13 +18,15 @@ export type AuthStackNavigationProp<T extends AuthStackScreen> = NavigationProp<
 >;
 export type AuthStackRouteProp<T extends AuthStackScreen> = RouteProp<AuthStackParamList, T>;
 
-const RootStack = createStackNavigator<AuthStackParamList>();
+const AuthStack = createStackNavigator<AuthStackParamList>();
 
 const Navigation: React.VFC = () => {
   return (
-    <RootStack.Navigator initialRouteName="AuthTop">
-      <RootStack.Screen name="AuthTop" component={AuthScreen} />
-    </RootStack.Navigator>
+    <AuthStack.Navigator initialRouteName="AuthTop">
+      <AuthStack.Screen name="AuthTop" component={AuthScreen} />
+      <AuthStack.Screen name="Signup" component={SignupScreen} />
+      <AuthStack.Screen name="Signin" component={SigninScreen} />
+    </AuthStack.Navigator>
   );
 };
 
